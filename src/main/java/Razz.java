7823704553d1030c53e90886289aee5a25aae98f
Rasmus Rudling <rasmusrudling@gmail.com>
2024-04-05 18:08:12 +0200

    feat: v1

--- src/main/java/Razz.java
+++ src/main/java/Razz.java
@@ -1,55 +1,76 @@
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
 
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+public class Razz extends Robot {
+    double previousEnergy = 100;
+    int movementDirection = 1;
+    int gunDirection = 1;
 
-/**
- * Razz - a robot by (your name here)
- */
-public class Razz extends Robot
-{
-	/**
-	 * run: Razz's default behavior
-	 */
-	public void run() {
-		// Initialization of the robot should be put here
+    public void run() {
+        setColors(Color.red, Color.blue, Color.green); // body,gun,radar
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
+        while (true) {
+            turnGunRight(360 * gunDirection);
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+            // Change movementDirection if we've seen a drop in energy.
+            double changeInEnergy = previousEnergy - getEnergy();
+            if (changeInEnergy > 0 && changeInEnergy <= 3) {
+                movementDirection = -movementDirection;
+                setAhead((Math.random() * 100 + 100) * movementDirection);
+            }
+            previousEnergy = getEnergy();
+        }
+    }
 
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
-		}
-	}
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double firePower = Math.min(500 / e.getDistance(), 3);
+        double bulletSpeed = 20 - firePower * 3;
+        long time = (long)(e.getDistance() / bulletSpeed); // Calculates how long the bullet will take to reach the target
+        double futureX = e.getVelocity() * Math.sin(e.getHeadingRadians()) * time; // Predicts the future x coordinate of the enemy
+        double futureY = e.getVelocity() * Math.cos(e.getHeadingRadians()) * time; // Predicts the future y coordinate of the enemy
+        double absoluteDegree = absoluteBearing(getX(), getY(), futureX, futureY);
+        turnGunRight(normalizeBearing(absoluteDegree - getGunHeading()));
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
-	}
+        fire(firePower);
+        gunDirection = -gunDirection;
+        scan(); // Might catch another robot while waiting for the bullet to travel
+    }
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
-	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+    public void onHitByBullet(HitByBulletEvent e) {
+        movementDirection = -movementDirection;
+        setAhead((Math.random() * 100 + 100) * movementDirection);
+    }
+
+    public void onHitWall(HitWallEvent e) {
+        movementDirection = -movementDirection;
+        setAhead((Math.random() * 100 + 100) * movementDirection);
+    }
+
+    // Normalizes a bearing to between +180 and -180
+    double normalizeBearing(double angle) {
+        while (angle > 180) angle -= 360;
+        while (angle < -180) angle += 360;
+        return angle;
+    }
+
+    // Calculates the absolute bearing between two points
+    double absoluteBearing(double x1, double y1, double x2, double y2) {
+        double xo = x2 - x1;
+        double yo = y2 - y1;
+        double hyp = Point2D.distance(x1, y1, x2, y2);
+        double arcSin = Math.toDegrees(Math.asin(xo / hyp));
+        double bearing = 0;
+
+        if (xo > 0 && yo > 0) { // both pos: lower-Left
+            bearing = arcSin;
+        } else if (xo < 0 && yo > 0) { // x neg, y pos: lower-right
+            bearing = 360 + arcSin; // arcsin is negative here, actually 360 - ang
+        } else if (xo > 0 && yo < 0) { // x pos, y neg: upper-left
+            bearing = 180 - arcSin;
+        } else if (xo < 0 && yo < 0) { // both neg: upper-right
+            bearing = 180 - arcSin; // arcsin is negative here, actually 180 + ang
+        }
+
+        return bearing;
+    }
 }
